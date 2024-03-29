0cca3ad320d227bcc072cc5d17d8a465fc49d19d
bomjidlo <159650768+bomjidlo@users.noreply.github.com>
2024-03-29 21:53:38 +0200

    Add files via upload

--- ping_pong.py
+++ ping_pong.py
@@ -7,8 +7,8 @@ font.init()
 
 clock = time.Clock()
 FPS = 60
-w = 700
-h = 500
+w = 600
+h = 600
 speed = 5
 window = display.set_mode((w, h))
 display.set_caption("Ping-pong")
@@ -28,22 +28,60 @@ class GameSprite(sprite.Sprite):
 
 
 class Player(GameSprite):
-    def update (self):
-        if key_pressed[K_d] and self.rect.x < 625:
-            self.rect.x += self.speed
-        if key_pressed[K_a] and self.rect.x > 5:
-            self.rect.x -= self.speed
-        if key_pressed[K_w] and self.rect.y > 250:
+    def update_1 (self):
+        if key_pressed[K_w] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if key_pressed[K_s] and self.rect.y < 425:
+        if key_pressed[K_s] and self.rect.y < 480:
             self.rect.y += self.speed
+    
+    def update_2 (self):
+        if key_pressed[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if key_pressed[K_DOWN] and self.rect.y < 480:
+            self.rect.y += self.speed
+        
+rocket_r = Player("ракетка.png", 30, 200, 10, 45, 120)    
+rocket_l = Player("ракетка.png", 520 , 200, 10, 45, 120)  
+ball = GameSprite("ball.png", 250, 250, 3, 50, 50 )  
+
+speed_x = 3 
+speed_y = 3
+
+blue = (0, 0, 255)
+green = (0, 255, 0)
+
+font1 = font.Font(None, 50)
+lose1 = font1.render("PLAYER 1 LOST!", True, blue)
+lose2= font1.render("PLAYER 2 LOST!", True, blue)
 
 running = True
+finish = False
 while running:
+    window.fill(background)
     for e in event.get(): 
         if e.type == QUIT:
                 running = False
     key_pressed = key.get_pressed()
+    if finish != True:
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+    if ball.rect.y > w -50 or ball.rect.y< 0:
+        speed_y *= -1
+    if sprite.collide_rect(rocket_r, ball) or sprite.collide_rect(rocket_l, ball):
+        speed_x *= -1
     
+    if ball.rect.x < 0:
+        finish = True
+        window.blit(lose1, (200, 300))
+
+    if ball.rect.x >= h:
+        finish = True
+        window.blit(lose2, (200, 300))
+    rocket_l.reset()
+    rocket_r.reset()
+    rocket_r.update_1()
+    rocket_l.update_2()
+    ball.reset()
+
     display.update()    
     clock.tick(FPS)
